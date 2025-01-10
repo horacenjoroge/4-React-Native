1574673ff97876a6dcfe0612d98fae1376422c71
“horacenjoroge” <horacenjorge@gmail.com>
2025-01-11 02:10:18 +0300

    first commit

---
+++ nucampsite/screens/MainComponent.js
@@ -0,0 +1,10 @@
+import { useState } from 'react';
+import { CAMPSITES } from '../shared/campsites.js';
+import DirectoryScreen from './DirectoryScreen';
+
+const Main = () => {
+    const [campsites, setCampsites] = useState(CAMPSITES);
+    return <DirectoryScreen campsites={campsites} />;
+};
+
+export default Main;
