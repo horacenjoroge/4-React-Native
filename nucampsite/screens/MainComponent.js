bf61a2af6d5d2ee10eaa51cd4a35557c580d4163
“horacenjoroge” <horacenjorge@gmail.com>
2025-01-11 09:55:49 +0300

    Components Part 2

--- nucampsite/screens/MainComponent.js
+++ nucampsite/screens/MainComponent.js
@@ -1,10 +1,26 @@
+import { View } from 'react-native';
 import { useState } from 'react';
 import { CAMPSITES } from '../shared/campsites.js';
 import DirectoryScreen from './DirectoryScreen';
+import CampsiteInfoScreen from './CampsiteInfoScreen';
 
 const Main = () => {
-    const [campsites, setCampsites] = useState(CAMPSITES);
-    return <DirectoryScreen campsites={campsites} />;
+   const [campsites, setCampsites] = useState(CAMPSITES);
+   const [selectedCampsiteId, setSelectedCampsiteId] = useState();
+
+   return (
+       <View style={{ flex: 1 }}>
+           <DirectoryScreen 
+               campsites={campsites} 
+               onPress={campsiteId => setSelectedCampsiteId(campsiteId)}
+           />
+           <CampsiteInfoScreen 
+               campsite={campsites.filter(
+                   campsite => campsite.id === selectedCampsiteId
+               )[0]}
+           />
+       </View>
+   );
 };
 
 export default Main;
