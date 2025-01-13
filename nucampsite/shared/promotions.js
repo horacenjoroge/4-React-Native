0779b6bbb614a84de7540affeaea41f231d81afe
“horacenjoroge” <horacenjorge@gmail.com>
2025-01-13 20:55:50 +0300

    Navigation Part 3

---
+++ nucampsite/shared/promotions.js
@@ -0,0 +1,11 @@
+export const PROMOTIONS = [
+    {
+        id: 0,
+        name: 'Mountain Adventure',
+        image: require("../asset/images/breadcrumb-trail-thumb.jpg"),
+        featured: true,
+        description:
+            'Book a 5-day mountain trek with a seasoned outdoor guide! Fly fishing equipment and lessons provided.',
+    },
+];
+
