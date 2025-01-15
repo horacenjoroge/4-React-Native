61108d2898de12ebc09afe1ae26b7b475effde0f
“horacenjoroge” <horacenjorge@gmail.com>
2025-01-15 19:53:31 +0300

    Activity Indicator

--- nucampsite/screens/AboutScreen.js
+++ nucampsite/screens/AboutScreen.js
@@ -1,51 +1,73 @@
-// AboutScreen.js
 import { ScrollView, Text } from 'react-native';
 import { Card, ListItem, Avatar } from 'react-native-elements';
 import { useSelector } from 'react-redux';
 import { baseUrl } from '../shared/baseUrl';
+import Loading from '../components/LoadingComponent';
 
-// Mission component remains unchanged as it doesn't depend on any external data
 const Mission = () => {
-    return (
-        <Card>
-            <Card.Title>Our Mission</Card.Title>
-            <Card.Divider />
-            <Text style={{ margin: 10 }}>
-                We present a curated database of the best campsites in the vast woods and backcountry of the World Wide Web Wilderness. We increase access to adventure for the public while promoting safe and respectful use of resources. The expert wilderness trekkers on our staff personally verify each campsite to make sure that they are up to our standards. We also present a platform for campers to share reviews on campsites they have visited with each other.
-            </Text>
-        </Card>
-    );
+   return (
+       <Card>
+           <Card.Title>Our Mission</Card.Title>
+           <Card.Divider />
+           <Text style={{ margin: 10 }}>
+               We present a curated database of the best campsites in the vast woods and backcountry of the World Wide Web Wilderness. We increase access to adventure for the public while promoting safe and respectful use of resources. The expert wilderness trekkers on our staff personally verify each campsite to make sure that they are up to our standards. We also present a platform for campers to share reviews on campsites they have visited with each other.
+           </Text>
+       </Card>
+   );
 };
 
 const AboutScreen = () => {
-    // Instead of local state, we now get partners data from Redux store
-    const partners = useSelector((state) => state.partners);
+   const partners = useSelector((state) => state.partners);
 
-    return (
-        <ScrollView>
-            <Mission />
-            <Card>
-                <Card.Title>Community Partners</Card.Title>
-                <Card.Divider />
-                {/* Map over partnersArray instead of partners directly */}
-                {partners.partnersArray.map((partner) => (
-                    <ListItem key={partner.id}>
-                        {/* Update Avatar source to use baseUrl for server images */}
-                        <Avatar 
-                            rounded 
-                            source={{ uri: baseUrl + partner.image }} 
-                        />
-                        <ListItem.Content>
-                            <ListItem.Title>{partner.name}</ListItem.Title>
-                            <ListItem.Subtitle>
-                                {partner.description}
-                            </ListItem.Subtitle>
-                        </ListItem.Content>
-                    </ListItem>
-                ))}
-            </Card>
-        </ScrollView>
-    );
+   if (partners.isLoading) {
+       return (
+           <ScrollView>
+               <Mission />
+               <Card>
+                   <Card.Title>Community Partners</Card.Title>
+                   <Card.Divider />
+                   <Loading />
+               </Card>
+           </ScrollView>
+       );
+   }
+
+   if (partners.errMess) {
+       return (
+           <ScrollView>
+               <Mission />
+               <Card>
+                   <Card.Title>Community Partners</Card.Title>
+                   <Card.Divider />
+                   <Text>{partners.errMess}</Text>
+               </Card>
+           </ScrollView>
+       );
+   }
+
+   return (
+       <ScrollView>
+           <Mission />
+           <Card>
+               <Card.Title>Community Partners</Card.Title>
+               <Card.Divider />
+               {partners.partnersArray.map((partner) => (
+                   <ListItem key={partner.id}>
+                       <Avatar 
+                           rounded 
+                           source={{ uri: baseUrl + partner.image }} 
+                       />
+                       <ListItem.Content>
+                           <ListItem.Title>{partner.name}</ListItem.Title>
+                           <ListItem.Subtitle>
+                               {partner.description}
+                           </ListItem.Subtitle>
+                       </ListItem.Content>
+                   </ListItem>
+               ))}
+           </Card>
+       </ScrollView>
+   );
 };
 
 export default AboutScreen;
--- nucampsite/screens/HomeScreen.js
+++ nucampsite/screens/HomeScreen.js
@@ -2,13 +2,26 @@ import { View, Text, ScrollView } from 'react-native';
 import { Card } from 'react-native-elements';
 import { useSelector } from 'react-redux';
 import { baseUrl } from '../shared/baseUrl';
+import Loading from '../components/LoadingComponent';
 
-// FeaturedItem component handles displaying individual featured items
-const FeaturedItem = ({ item }) => {
+const FeaturedItem = (props) => {
+   const { item } = props;
+
+   if (props.isLoading) {
+       return <Loading />;
+   }
+
+   if (props.errMess) {
+       return (
+           <View>
+               <Text>{props.errMess}</Text>
+           </View>
+       );
+   }
+
    if (item) {
        return (
            <Card containerStyle={{ padding: 0 }}>
-               {/* Using baseUrl to construct complete image URL */}
                <Card.Image source={{ uri: baseUrl + item.image }}>
                    <View style={{ justifyContent: 'center', flex: 1 }}>
                        <Text 
@@ -30,23 +43,31 @@ const FeaturedItem = ({ item }) => {
 };
 
 const HomeScreen = () => {
-   // Using useSelector to access data from Redux store
    const campsites = useSelector((state) => state.campsites);
    const promotions = useSelector((state) => state.promotions);
    const partners = useSelector((state) => state.partners);
 
-   // Finding featured items from each category
    const featCampsite = campsites.campsitesArray.find((item) => item.featured);
-   const featPromotion = promotions.promotionsArray.find(
-       (item) => item.featured
-   );
+   const featPromotion = promotions.promotionsArray.find((item) => item.featured);
    const featPartner = partners.partnersArray.find((item) => item.featured);
 
    return (
        <ScrollView>
-           <FeaturedItem item={featCampsite} />
-           <FeaturedItem item={featPromotion} />
-           <FeaturedItem item={featPartner} />
+           <FeaturedItem
+               item={featCampsite}
+               isLoading={campsites.isLoading}
+               errMess={campsites.errMess}
+           />
+           <FeaturedItem
+               item={featPromotion}
+               isLoading={promotions.isLoading}
+               errMess={promotions.errMess}
+           />
+           <FeaturedItem
+               item={featPartner}
+               isLoading={partners.isLoading}
+               errMess={partners.errMess}
+           />
        </ScrollView>
    );
 };
