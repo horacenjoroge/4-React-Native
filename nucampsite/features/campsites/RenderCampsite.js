ffb09983fc4630dcdace3f17c3a9e7899cc0c056
“horacenjoroge” <horacenjorge@gmail.com>
2025-01-25 02:56:37 +0300

    Social Sharing

--- nucampsite/features/campsites/RenderCampsite.js
+++ nucampsite/features/campsites/RenderCampsite.js
@@ -1,11 +1,19 @@
 import { useRef } from 'react';
-import { StyleSheet, Text, View, PanResponder, Alert } from 'react-native';
+import {
+    StyleSheet,
+    Text,
+    View,
+    PanResponder,
+    Alert,
+    Share
+} from 'react-native';
 import { Card, Icon } from 'react-native-elements';
 import { baseUrl } from '../../shared/baseUrl';
 import * as Animatable from 'react-native-animatable';
 
 const RenderCampsite = (props) => {
     const { campsite } = props;
+
     const view = useRef();
 
     const isLeftSwipe = ({ dx }) => dx < -200;
@@ -50,6 +58,19 @@ const RenderCampsite = (props) => {
         }
     });
 
+    const shareCampsite = (title, message, url) => {
+        Share.share(
+            {
+                title,
+                message: `${title}: ${message} ${url}`,
+                url
+            },
+            {
+                dialogTitle: 'Share ' + title
+            }
+        );
+    };
+
     if (campsite) {
         return (
             <Animatable.View
@@ -86,6 +107,20 @@ const RenderCampsite = (props) => {
                             raised
                             reverse
                             onPress={props.onShowModal}
+                        />
+                        <Icon
+                            name='share'
+                            type='font-awesome'
+                            color='#5637DD'
+                            raised
+                            reverse
+                            onPress={() =>
+                                shareCampsite(
+                                    campsite.name,
+                                    campsite.description,
+                                    baseUrl + campsite.image
+                                )
+                            }
                         />
                     </View>
                 </Card>
