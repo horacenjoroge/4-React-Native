7c23e304cb0266d22a759d8f9ef9fb9ff8657e33
“horacenjoroge” <horacenjorge@gmail.com>
2025-01-28 10:38:23 +0300

    Network Info

--- nucampsite/redux/store.js
+++ nucampsite/redux/store.js
@@ -1,7 +1,7 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { campsitesReducer } from '../features/campsites/campsitesSlice';
 import { commentsReducer } from '../features/comments/commentsSlice';
-import { partnersReducer } from '../features/patners/partnersSlice';
+import { partnersReducer } from '../features/partners/partnersSlice';
 import { promotionsReducer } from '../features/promotions/promotionsSlice';
 import { favoritesReducer } from '../features/favorites/favoritesSlice';
 import {
